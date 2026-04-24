5c929d7a4b18edd7bf004f26b9e990de66491f94
Vaijnath Patil <vaijuwalker111@gmail.com>
2026-04-24 11:02:24 +0530

    Refactor main function to improve argument handling and file reading

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
 use std :: env;
+use std::fs;
+use std ::process;
+
 
 fn main() {
     let args:Vec<String> = env::args().collect();
 
+
+    if args.len() != 3{
+        eprintln!("Usage:minigrep <query> <file_path>");
+        process::exit(1);
+    }
+
     let query = &args[1];
-    let filename = &args[2];
+    let file_path = &args[2];
 
 
     println!("Searching for {}",query);
-    println!("In file {}",filename);
+    println!("In file {}",file_path);
+
+    let contents = fs::read_to_string(file_path).unwrap_or_else(|err|{
+        eprintln!("Failed to read file  '{}'  {}",file_path,err);
+        process::exit(1);
+    });
+     
+    println!("With text:\n{}",contents);
+
 }
+
+
